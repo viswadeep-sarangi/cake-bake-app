92ad3f21678b0390976f2516422711c39dc8c5c4
Viswadeep Sarangi <viswadeep.sarangi@gmail.com>
2022-08-20 22:05:11 +0100

    basic db setup complete

--- src/cake-bake-app/config.py
+++ src/cake-bake-app/config.py
@@ -2,6 +2,11 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
 
+    db_name:str
+    db_schema:str
+
     class Config:
             env_file = '.env'
             env_file_encoding = 'utf-8'
+
+config = Settings()
---
+++ src/cake-bake-app/db.py
@@ -0,0 +1,49 @@
+import re
+from typing import Any
+from venv import create
+from sqlalchemy import create_engine, MetaData, Column, String, Table, DateTime, ForeignKey, Boolean
+from sqlalchemy.ext.declarative import as_declarative, declared_attr
+from sqlalchemy.engine import Engine
+from config import config
+import logging
+
+_logger = logging.getLogger(__name__)
+
+@as_declarative()
+class Base(object):
+    __table_args__ = {"schema": config.db_schema}
+
+    @declared_attr
+    def __tablename__(cls: Any) -> str:
+        """Takes the name of the Class with CamelCase and converts it into snake_case. This means we
+        can use the class name.
+
+        Returns:
+            [type]: [description]
+
+        """
+        return re.sub("(?!^)([A-Z]+)", r"_\1", cls.__name__).lower()
+
+class Employees(Base):
+    name = Column(String, primary_key=True)
+    date_of_birth = Column(DateTime)
+
+class CakePreferences(Base):
+    name = Column(String, ForeignKey(f"{config.db_schema}.employees.name"), primary_key=True)
+    food_intolerence = Column(String)
+    cake_preference = Column(String)
+    can_bake = Column(Boolean)
+    can_provide_ingredients = Column(String)
+    
+def create_db_engine(name:str = config.db_name)->Engine:
+    engine = create_engine(f"sqlite:///{name}.db", echo=True)
+    return engine
+
+def create_db_schema(engine:Engine):
+    _logger.info(f"Creating DB schema for engine {engine}")
+    Base.metadata.create_all(engine)
+
+# for debugging purposes only
+if __name__=="__main__":
+    engine = create_db_engine()
+    create_db_schema(engine)
